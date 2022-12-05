57a77b12f0262c03e62573d596648f9e4615c64c
HIKMATFATHURRAHMAN <hikmatfathurrahman5@gmail.com>
2022-12-05 16:18:36 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercis4
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
