51b5402d1b82d7a324b3f3cf165dc61f0aca9278
HIKMATFATHURRAHMAN <hikmatfathurrahman5@gmail.com>
2022-12-05 16:34:52 +0700

    add static void main

--- Program.cs
+++ Program.cs
@@ -67,4 +67,45 @@ namespace exercis4
         }
     }
 
+    static void Main(string[] args)
+    {
+        stack s = new stack();
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\n***Stack Menu***\n");
+            Console.WriteLine("1. Push ");
+            Console.WriteLine("2. Pop ");
+            Console.WriteLine("3. Display ");
+            Console.WriteLine("4. exit");
+            Console.WriteLine("\nEnter your choice ");
+            string Input = Console.ReadLine();
+            char ch = Convert.ToChar(Input == "" ? "0" : Input);
+            switch (ch)
+            {
+                case '1':
+                    Console.Write("\nEnter a number :");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    s.push(num);
+                    break;
+                case '2':
+                    if (s.empty())
+                    {
+                        Console.WriteLine("\nSTack empty");
+                        break;
+
+                    }
+                    s.pop();
+                    break;
+                case '3';
+                    s.display();
+                case '4':
+                    return;
+                default:
+                    Console.WriteLine("\nInvalid Choice");
+                    break;
+
+            }
+        }
+    }
 }
